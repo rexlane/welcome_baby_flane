d9b0a6a561a4bfdbe34e03f4e2b61b6367eddf98
rexlane <rex.m.lane@gmail.com>
2017-10-20 11:56:14 -0500

    initiate -wip branch

--- js/loadpagetabletop.js
+++ js/loadpagetabletop.js
@@ -31,17 +31,56 @@ function loadTemplates() {
   var context_length = context.body.length;
   var random_index = Math.floor((Math.random() * context_length) +1);
   var random_word = context.body[random_index]
+  var current_color_name = random_word.word;
   var current_color = random_word.definition;
-  console.log(current_color)
+/*  console.log(current_color)*/
    $("body").css("background-color",current_color);
+   $(".wordTitle").css("color","orange")
     $("#content").html(template(random_word));
-  //  $(".wordTitle").css("color":)
+    $("#claim_form").hide();
+    loadNext(random_index);
+/*
+    $('#claim_form').submit(
+        function() {
+          var inputs = $('#claim_form :input');
+          console.log(inputs);
+  });*/
 };
 
-function showExample() {
-  $("#example-chunk").toggle();
+
+/*returns random index and calls findColor*/
+function randomIndex() {
+  var context_length = context.body.length;
+  var random_index = Math.floor((Math.random() * context_length) +1);
+  findWord(random_index);
 }
 
-function showDef() {
-  $("#def-chunk").toggle();
-};
+/*returns next index and calls findWord*/
+function nextIndex(current_index) {
+    var next_index = current_index + 1;
+    findWord(next_index);
+}
+
+
+function findWord(index) {
+  var chosen_word = context.body[index]
+  var current_color_name = random_word.word;
+  var current_color = random_word.definition;
+}
+
+function putWord(chosen_word) {
+
+}
+
+
+
+
+
+function showForm() {
+  $("#claim_form").toggle();
+}
+
+
+function submitClaimForm() {
+  console.log(this)
+}
