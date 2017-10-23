c1a8d022c9ccb01dfb29bc818c057ad338b5b5a3
rexlane <rex.m.lane@gmail.com>
2017-10-23 09:19:10 -0500

    makes it talk to google sheets
    to-dos:
    
    *stop page from refreshing after form submittion
    *make form pretty
    *append names to color list instead of separate sheet?
    *limit color list to unclaimed colors
    *create confirmation/THANK YOU msg
    *make colors scroll forward instead of jump randomly

--- js/loadpagetabletop.js
+++ js/loadpagetabletop.js
@@ -7,7 +7,8 @@ $( document ).ready(function() {
   var spreadsheet_url = 'https://docs.google.com/spreadsheets/d/1eZa-5mbMd61s-LV2IjIqWIaLGsRWpmX9vcDkUDmJW4c/pubhtml'
   initializeTabletopObject(spreadsheet_url);
   template = Handlebars.compile($("#sections-template").html());
-});
+/*  submitForm();
+*/});
 
 function initializeTabletopObject(dataSpreadsheet){
   Tabletop.init({
@@ -36,16 +37,18 @@ function loadTemplates() {
 /*  console.log(current_color)*/
    $("body").css("background-color",current_color);
    $(".wordTitle").css("color","orange")
-    $("#content").html(template(random_word));
-    $("#claim_form").hide();
-    loadNext(random_index);
-/*
+   $("#content").html(template(random_word));
+   $("#claim_form").hide();
+/*    loadNext(random_index);
+*//*
     $('#claim_form').submit(
         function() {
           var inputs = $('#claim_form :input');
           console.log(inputs);
   });*/
+  return current_color;
 };
+
 
 
 /*returns random index and calls findColor*/
@@ -74,13 +77,73 @@ function putWord(chosen_word) {
 
 
 
+function submitForm() {
+  // Variable to hold request
+  var request;
+
+  // Bind to the submit event of our form
+/*  $("#claim_form").submit(function(event){
+  var request;
+  event.preventDefault();*/
+
+      // Prevent default posting of form - put here to work in case of errors
+/*      event.preventDefault();
+
+      // Abort any pending request
+      if (request) {
+          request.abort();
+      }
+      // setup some local variables
+/*      var $form = $(this);
+*/      var $form = $("#claim_form");
+
+      // Let's select and cache all the fields
+      var $inputs = $form.find("input, select, button, textarea");
+
+      // Serialize the data in the form
+      var serializedData = $form.serialize();
+      serializedData = serializedData + "&color=" + loadTemplates();
+      // Let's disable the inputs for the duration of the Ajax request.
+      // Note: we disable elements AFTER the form data has been serialized.
+      // Disabled form elements will not be serialized.
+      $inputs.prop("disabled", true);
+
+      console.log(serializedData);
+/*      console.log(loadTemplates());
+*/      // Fire off the request to /form.php
+      request = $.ajax({
+          url: "https://script.google.com/macros/s/AKfycby5HWQFUFs2iR--xuidcgCzsoj4mNt6wkdy53U5XazYVasNv70/exec",
+          type: "post",
+          data: serializedData
+      });
+
+      // Callback handler that will be called on success
+      request.done(function (response, textStatus, jqXHR){
+          // Log a message to the console
+          console.log("Hooray, it worked!");
+      });
+
+      // Callback handler that will be called on failure
+      request.fail(function (jqXHR, textStatus, errorThrown){
+          // Log the error to the console
+          console.error(
+              "The following error occurred: "+
+              textStatus, errorThrown
+          );
+      });
+
+      // Callback handler that will be called regardless
+      // if the request failed or succeeded
+      request.always(function () {
+          // Reenable the inputs
+          $inputs.prop("disabled", false);
+      });
+
+/*  });
+*/}
+
 
 
 function showForm() {
   $("#claim_form").toggle();
-}
-
-
-function submitClaimForm() {
-  console.log(this)
 }
